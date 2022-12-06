24671aa52e3c92f46e93a10ef34afdd7dbc70ffc
LENOVO <LENOVO@DESKTOP-OR8J65R>
2022-12-06 14:27:28 +0530

    second commit

---
+++ gitPro/src/test/java/gitPack1/TestClass2.java
@@ -0,0 +1,23 @@
+package gitPack1;
+
+public class TestClass2 {
+
+	public static void main(String[] args) 
+	{
+		System.out.println("test1");
+		System.out.println("test1");
+		System.out.println("test1");
+		System.out.println("test1");
+		System.out.println("test1");
+		System.out.println("test1");
+		System.out.println("test1");
+		System.out.println("test1");
+		System.out.println("test1");
+
+
+
+		
+
+	}
+
+}
