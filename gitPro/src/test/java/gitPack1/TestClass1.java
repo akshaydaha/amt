f4882534e6dc9cbea581e953066070531f2899b6
LENOVO <LENOVO@DESKTOP-OR8J65R>
2022-12-06 12:33:57 +0530

    First commit

---
+++ gitPro/src/test/java/gitPack1/TestClass1.java
@@ -0,0 +1,17 @@
+package gitPack1;
+
+public class TestClass1 {
+
+	public static void main(String[] args) 
+	{
+		System.out.println("TC01");
+		System.out.println("TC02");
+		System.out.println("TC03");
+		System.out.println("TC04");
+		System.out.println("TC05");
+		System.out.println("TC06");
+		
+		
+	}
+
+}
