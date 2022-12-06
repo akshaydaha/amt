4df441ea2aa2c22b0aa2d4c42d8617472a55e275
akshaydaha <119919410+akshaydaha@users.noreply.github.com>
2022-12-06 14:18:02 +0530

    Update TestClass1.java

--- gitPro/src/test/java/gitPack1/TestClass1.java
+++ gitPro/src/test/java/gitPack1/TestClass1.java
@@ -5,6 +5,11 @@ public class TestClass1 {
 	public static void main(String[] args) 
 	{
 		System.out.println("TC01");
+		System.out.println("TC01");
+		System.out.println("TC01");
+		System.out.println("TC01");
+		System.out.println("TC01");
+		System.out.println("TC01");
 		
 		
 	}
